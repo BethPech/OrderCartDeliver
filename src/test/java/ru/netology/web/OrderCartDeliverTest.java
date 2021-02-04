472f716e12588764cdce4e92c9fdbe1884269f7d
BethPech <colnze17-94@mail.ru>
2021-02-04 22:17:37 +0300

    All tests passed

--- src/test/java/ru/netology/web/OrderCartDeliverTest.java
+++ src/test/java/ru/netology/web/OrderCartDeliverTest.java
@@ -26,6 +26,7 @@ public class OrderCartDeliverTest {
         $(withText("Успешно!")).shouldBe(exist);
         $(byText("Успешно!")).waitUntil(visible, 11000);
     }
+
     @Test
     void shouldFailTestBecauseOfLowCaseLetters() {
         open("http://localhost:9999");
@@ -35,112 +36,6 @@ public class OrderCartDeliverTest {
         $("[name='name']").setValue("ванов-етров авел");
         $("[name='phone']").setValue("+98765432145");
         $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldNotSubmitRequestBecauseOfWrongCity() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Городец");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Павел");
-        $("[name='phone']").setValue("+98765432145");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-
-
-
-    @Test
-    void shouldNotSubmitRequestBecauseOfEmptyCity() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Павел");
-        $("[name='phone']").setValue("+98765432145");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldFailBecauseOfEnglish() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Pavel");
-        $("[name='phone']").setValue("+98765432145");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldFailBecauseOfWrongName() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("7896574596");
-        $("[name='phone']").setValue("+98765432145");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldFailBecauseOfWrongNumberWithoutPlus() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Павел");
-        $("[name='phone']").setValue("98765432145");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldFailBecauseOfALotOfNumbers() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Павел");
-        $("[name='phone']").setValue("+987654321459");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldFailBecauseOfALittleOfNumbers() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Павел");
-        $("[name='phone']").setValue("+98765432");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).waitUntil(visible, 11000);
-    }
-    @Test
-    void shouldFailBecauseOfCheckboxAbsence() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").setValue(str);
-        $("[name='name']").setValue("Иванов-Петров Павел");
-        $("[name='phone']").setValue("+98765432145");
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(exist);
         $(byText("Успешно!")).waitUntil(visible, 11000);
