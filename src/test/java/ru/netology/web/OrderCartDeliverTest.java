37ded4925e7ec3de93995d1f48aad01ee47f0f98
BethPech <Colnze17-94@mail.ru>
2021-03-31 22:39:10 +0300

    Allure added

--- src/test/java/ru/netology/web/OrderCartDeliverTest.java
+++ src/test/java/ru/netology/web/OrderCartDeliverTest.java
@@ -1,5 +1,9 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -11,6 +15,15 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderCartDeliverTest {
+
+    @BeforeAll
+            static void setUp(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+            static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
 
     Date date = new Date();
     String str = String.format("%tc", date);
